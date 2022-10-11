d7a11eced6c7b136e1fdf18bb210b31a7dab5ef1
julianmolina99 <juliandavidmv@ufps.edu.co>
2022-10-11 16:50:05 -0500

    model in Sequelize

--- services/user.service.js
+++ services/user.service.js
@@ -1,6 +1,7 @@
 const boom = require('@hapi/boom');
 
 const pool = require('../libs/postgres.pool');
+const { models } = require('./../libs/sequelize');
 
 class UserService {
   constructor() {
@@ -13,9 +14,11 @@ class UserService {
   }
 
   async find() {
-    const query = 'SELECT * FROM tasks';
-    const rta = await this.pool.query(query);
-    return rta.rows;
+    const rta = await models.User.findAll();
+    return rta;
+    // const query = 'SELECT * FROM tasks';
+    // const rta = await this.pool.query(query);
+    // return rta.rows;
   }
 
   async findOne(id) {
